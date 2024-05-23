97bcc48c09da1c5ba0b6717c322dc292be19a34b
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-05-23 09:06:48 +0200

    feat: Smoothing function for firing rates.
    - Implement mtcdb.preprocess.firing_rates.smooth().
    - Implement the corresponding test:
       test_mtcdb.test_preprocess.test_firing_rates.test_smooth()

--- src/mtcdb/preprocess/firing_rates.py
+++ src/mtcdb/preprocess/firing_rates.py
@@ -5,7 +5,6 @@
 Convert raw spike times to firing rates.
 """
 
-
 import numpy as np
 from scipy.signal import fftconvolve
 from typing import Any
@@ -34,40 +33,83 @@ def spikes_to_rates(spk: ArrayLike,
     -------
     frates: NumpyArray
         Firing rate time course (in spikes/s).
+        Shape: ``(ntpts, 1)`` with ``ntpts = tmax/tbin`` (number of bins).
     
     See Also
     --------
     numpy.histogram: 
-        Count the number of spikes in each bin.
-        Parameter ``bin``: Bin edges, *including the rightmost edge*.
-        Two outputs: ``hist`` (number of spikes in each bin), ``edges``.
+        Used to count the number of spikes in each bin.
+        Input: ``bin`` (bin edges, *including the rightmost edge*).
+        Outputs: ``hist`` (number of spikes in each bin), ``edges`` (useless).
     
     Notes
     -----
 
     Algorithm
 
-    - Divide the recording period  [0, ``tmax``] into bins of size ``tbin``.
+    - Divide the recording period  ``[0, tmax]`` into bins of size ``tbin``.
     - Count the number of spikes in each bin.
-    - Divide the number of spikes in each bin by the bin size ``tbin``.
+    - Divide the spikes count in each bin by the bin size ``tbin``.
 
-    Bin edges are obtained with :func:`numpy.arange`, 
-    with the last bin edge at ``tmax + tbin`` for the last bin to be included.
+    [Op1] Bin edges are obtained with :func:`numpy.arange`, 
+    with the last bin edge at ``tmax + tbin`` to include the last bin.
+    
+    [Op2] The output shape is has two dimensions representing
+    time (length ``n_bins``),
+    trials (length ``1``, single trial).
+    It ensures compatibility and consistence in the full process.
     """
-    frates = np.histogram(spk, bins=np.arange(0, tmax+tbin, tbin))[0]/tbin
+    frates = np.histogram(spk, bins=np.arange(0, tmax+tbin, tbin))[0]/tbin # [Op1]
+    frates = frates[:,np.newaxis] # [Op2]
     return frates
 
 
-def smooth(frates: Any,
+def smooth(frates: NumpyArray,
            window: float,
            tbin: float,
-           ) -> Any:
+           mode: str = 'valid',
+           ) -> NumpyArray:
     """
-    Smooth the firing rates in time.
+    Smooth the firing rates across time.
 
+    Parameters
+    ----------
+    frates: NumpyArray
+        Firing rate time course (in spikes/s).
+        Shape: ``(ntpts, ntrials)``,
+    window: float
+        Smoothing window size (in seconds).
+    tbin: float
+        Time bin (in seconds).
+    
+    Returns
+    -------
+    smoothed: NumpyArray
+        Smoothed firing rate time course (in spikes/s).
+        Shape: ``(ntpts_out, ntrials)``, ``ntpts_out`` depend on ``mode``.
+        With ``"valid"``:  ``ntpts_out = ntpts - window/tbin + 1``.
+        With ``"same"``:  ``ntpts_out = ntpts``.
+    
     See Also
     --------
-    scipy.signal.fftconvolve
+    scipy.signal.fftconvolve: Used to convolve the firing rate time course with a boxcar kernel.
+    
+    Notes
+    -----
+    Smoothing consists in averaging consecutive values in a sliding window.
+
+    Algorithm
+
+    - Convolve the firing rate time course with a boxcar kernel (FFT method).
+      Size of the window: ``window/tbin``.
+    - Divide the output by the window size to get the average.
+
+    Convolution Modes: ``'full'`` / ``'valid'`` / ``'same'``
+
+    - ``'same'``: Keep the output shape as the input sequence.
+    - ``'valid'``: Keep only the values which are not influenced by zero-padding.
     """
-    pass
+    kernel = np.ones((int(window/tbin), 1)) # add one dimension for shape compatibility
+    smoothed = fftconvolve(frates, kernel, mode=mode, axes=0)/len(kernel)
+    return smoothed
     
--- tests/test_mtcdb/test_preprocess/test_firing_rates.py
+++ tests/test_mtcdb/test_preprocess/test_firing_rates.py
@@ -10,15 +10,17 @@ Tests for the module :mod:`mtcdb.preprocess.firing_rates`.
 import numpy as np
 import pytest
 
-from mtcdb.preprocess.firing_rates import spikes_to_rates
+from mtcdb.preprocess.firing_rates import spikes_to_rates, smooth
+
 
 def test_bin_spikes():
-    """Test for :func:`mtcdb.preprocess.firing_rates.spikes_to_rates`.
+    """
+    Test for :func:`mtcdb.preprocess.firing_rates.spikes_to_rates`.
     
     Test Inputs
     -----------
     tmax: float
-        Set to 1.0, for a recording period spanning [0, 1].
+        Set to 1.0, for a recording period spanning ``[0, 1]``.
     tbin: float
         Set to 0.1, to divide the recording period into 10 bins.
     spk: numpy.ndarray
@@ -26,8 +28,9 @@ def test_bin_spikes():
     
     Expected Outputs
     ----------------
-    expected: numpy.ndarray
+    expected: NumpyArray
         10 values (10 bins) of 20 spikes/s.
+        Shape: ``(1, 10)`` (1 trial, 10 bins).
 	
     See Also
     --------
@@ -38,8 +41,52 @@ def test_bin_spikes():
     tmax = 1.0
     tbin = 0.1
     spk = np.linspace(0, tmax, num=20)
-    expected = np.full(shape=10, fill_value=20.0)
+    expected = np.full(shape=(10, 1), fill_value=20.0)
     frates = spikes_to_rates(spk, tbin, tmax)
     assert frates.shape == expected.shape, "Wrong shape"
     assert np.allclose(frates, expected), "Wrong values"
 
+
+
+def test_smooth():
+    """
+    Test for :func:`mtcdb.preprocess.firing_rates.smooth`.
+
+    Test Inputs
+    -----------
+    frates: NumpyArray
+        Shape: ``(10, 2)`` (10 bins, 2 trials).
+        Values: Two alternating patterns of 0 and 1, repeated 5 times (10 values).
+        Both patterns are similar but inverted (0, 1 vs. 1, 0).
+    tbin: float
+        Set to 0.1, for a recording period of ``10 * 0.1 = 1`` s.
+    window: int
+        Set to 0.2, for a boxcar kernel of 2 bins width.
+    mode: str
+        Set to ``'valid'``, to keep only the values which are not
+        influenced by zero-padding.
+
+    Expected Outputs
+    ----------------
+    expected: NumpyArray
+        Shape: ``(7, 2)`` (7 time bins, 2 trials).
+        In the ``'valid'`` mode, the output time dimension is given by:
+        ``ntpts_out = ntpts - window/tbin + 1``.
+        Here: ``window/tbin = 2`` bins, so ``ntpts_out = 10 - 2 + 1 = 9``.
+        Output values: All equal to 0.5, 
+        which is the average of two consecutive values in the input (0 and 1),
+        whatever their order in the patterns.
+    
+    See Also
+    --------
+    numpy.allclose
+    numpy.tile
+    """
+    frates = np.tile(np.array([[0, 1], [1, 0]]), (5, 1)) # shape: (10, 2)
+    tbin = 0.1
+    window = 0.2
+    mode = 'valid'
+    expected = np.full(shape=(9, 2), fill_value=0.5)
+    smoothed = smooth(frates, window, tbin, mode)
+    assert smoothed.shape == expected.shape, "Wrong shape"
+    assert np.allclose(smoothed, expected), "Wrong values"
