db18c6297989a1584e4e38309de8b77ab7f837e7
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-07-13 14:31:02 +0200

    feat: Initialize modules in source code and tests.

---
+++ src/mtcdb/preprocess/hierarchical_bootstrap.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+:mod:`mtcdb.preprocess.hierarchical_bootstrap` [module]
+
+Build pseudo-trials using hierarchical bootstrap.
+
+"""
+
+import numpy as np
+import numpy.typing as npt
---
+++ tests/test_mtcdb/test_preprocess/test_hierarchical_bootstrap.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+:mod:`test_mtcdb.test_preprocess.test_hierarchical_bootstrap` [module]
+
+See Also
+--------
+:mod:`mtcdb.preprocess.hierarchical_bootstrap`: Tested module.
+"""
+
+import numpy as np
+import numpy.typing as npt
+from numpy.testing import assert_array_almost_equal as assert_array_eq
+import pytest
