b259c4d212ca16234877936887479afc0e0eb48f
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-07-16 14:44:50 +0200

    test: Correct fixtures to generate data for tests - Use requests to ensure consistency between different fixtures. - Use `request.getfixturevalue` to get a fixture value instead of the fixture as a function itself during attribute assignment.
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

--- tests/test_mtcdb/test_data_structures/test_base.py
+++ tests/test_mtcdb/test_data_structures/test_base.py
@@ -17,111 +17,121 @@ from typing import Tuple, Mapping, FrozenSet
 import numpy as np
 import pytest
 
-from mtcdb.data_structures.base import MetaData, Data
+from mtcdb.data_structures.base import Data
+
 
 @pytest.fixture
-def test_dim2coord():
+def dim2coord():
     """
-    Fixture for the class attribute :attr:`dim2coord`.
-    
+    Fixture to generate the class attribute :attr:`dim2coord`.
+
     Returns
     -------
     MappingProxyType:
         Two dimensions: 'dim1', 'dim2'.
         One coordinate for 'dim1' and two for 'dim2'.
     """
-    return MappingProxyType({
-        'dim1': frozenset(['coord1']),
-        'dim2': frozenset(['coord2', 'coord3'])
-    })
+    return MappingProxyType(
+        {"dim1": frozenset(["coord1"]), "dim2": frozenset(["coord2", "coord3"])}
+    )
+
 
 @pytest.fixture
-def test_data() -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+def data(dim2coord) -> np.ndarray:
     """
-    Fixture for data and coordinates, consistent with :func:`dim2coord`.
+    Fixture to generate data consistent with :func:`dim2coord`.
 
     Returns
     -------
     data: np.ndarray
-        Data array with two dimensions.
-    coord1, coord2, coord3: np.ndarray
-        Coordinate arrays for 'dim1' and 'dim2', with appropriate lengths.
+        Array full of O with length 3 along each dimensions specified in :attr:`dim2coord`.
     """
-    shape = (3, 3)
+    n_dims = len(dim2coord)
+    shape = tuple(3 for _ in range(n_dims))
     data = np.zeros(shape)
-    coord1 = np.ones(shape[0])
-    coord2 = np.ones(shape[1])
-    coord3 = np.ones(shape[1])
-    return data, coord1, coord2, coord3
+    return data
+
 
 @pytest.fixture
-def test_class(test_dim2coord):
+def coords(dim2coord, data) -> Tuple[np.ndarray, ...]:
     """
-    Fixture which creates a test class inheriting from :class:`Data`.
+    Fixture to generate coordinates consistent with :func:`dim2coord` and :func:`data`.
+
+    Returns
+    -------
+    coord1, coord2...: np.ndarray
+        Arrays associated to each dimension, with appropriate lengths.
+    """
+    coords = []
+    for i, (dim, coord_set) in enumerate(dim2coord.items()):
+        for c in coord_set:  # all coordinates associated to the dimension
+            coords.append(np.arange(data.shape[i]))
+    return tuple(coords)
+
+
+@pytest.fixture
+def subclass(request):
+    """
+    Fixture to create a test class inheriting from :class:`Data`.
 
     Returns
     -------
     TestClass:
         Class inheriting from :class:`Data`.
-        It has the class attribute :attr:`dim2coord`.
-        It implements the property :attr:`path` 
-        which is required by the abstract base class.
-    
+        It defines the class attribute :attr:`dim2coord` required by the metaclass :class:`MetaData`.
+        It implements the property :attr:`path` required by the abstract base class :class:`Data`.
+
     See Also
     --------
     :func:`dim2coord`: Requested fixture for :attr:`dim2coord`.
+    :func:`request.getfixturevalue`: Method of the `request` fixture used to retrieve the *value*
+        returned by the :func:`dim2coord` fixture. Otherwise, the fixture would be passed as a
+        function during attribute assignment, leading to an AttributeError.
     """
+
     class TestClass(Data):
-        dim2coord = test_dim2coord
+        dim2coord = request.getfixturevalue("dim2coord")
 
         @property
         def path(self):
-            return 'path/to/data'
+            return "path/to/data"
 
     return TestClass
 
 
-@pytest.mark.parametrize("approach",
-                        argvalues=["MetaClass", "BaseClass"],
-                        ids=["MetaClass", "BaseClass"])
-def test_class_creation(approach, test_dim2coord, test_class):
+def test_class_creation(dim2coord, subclass):
     """
-    Test :class:`MetaData` and inheritance from :class:`Data`.
+    Test subclass creation involving :class:`MetaData` and :class:`Data`.
 
     Test Inputs
     -----------
-    Test classes with the class attribute :attr:`dim2coord`.
-    TestClass [MetaData]: 
-        Using the metaclass :class:`MetaData`.
-    TestClass [Data]:
-        Using inheritance from :class:`Data`.
-    
+    Test class defined with the class attribute :attr:`dim2coord`.
+
     Expected Output
     ---------------
-    Creation of the attributes :attr:`dims`, :attr:`coords` and :attr:`coord2dim`.
-    Those objects should be consistent with the definition of :attr:`dim2coord`.
+    Class attributes :attr:`dims`, :attr:`coords` and :attr:`coord2dim` should be set automatically
+    and be consistent with the content of :attr:`dim2coord`.
 
     See Also
     --------
     :func:`dim2coord`: Requested fixture for :attr:`dim2coord`.
-    :func:`test_class`: Requested fixture a class created via the base class approach.
+    :func:`test_class`: Requested fixture creating a sub-class created via inheritance.
     """
-    # Dynamically create the classes for testing
-    if approach == "MetaClass": # override the test class
-        class TestClass(metaclass=MetaData):
-            dim2coord = test_dim2coord
-    elif approach == "BaseClass": # already created by the fixture
-        TestClass = test_class
-    expected_dims = ('dim1', 'dim2')
-    expected_coords = ('coord1', 'coord2', 'coord3')
-    expected_coord2dim = MappingProxyType({
-        'coord1': 'dim1',
-        'coord2': 'dim2',
-        'coord3': 'dim2',
-    })
-    assert TestClass.dims == expected_dims
-    assert TestClass.coords == expected_coords
-    assert TestClass.coord2dim == expected_coord2dim
+    # Check the presence of the class attributes
+    assert hasattr(subclass, "dims")
+    assert hasattr(subclass, "coords")
+    assert hasattr(subclass, "coord2dim")
+    # Check that the dimensions are set correctly
+    expected_dims = tuple(dim2coord.keys())
+    assert subclass.dims == expected_dims
+    # Check that all the coordinates are present in the class attribute
+    expected_coords = set([c for coord_set in dim2coord.values() for c in coord_set])
+    for c in expected_coords:
+        assert c in expected_coords
+    # Check that the mapping is correct
+    for dim, coord_set in dim2coord.items():
+        for c in coord_set:
+            assert subclass.coord2dim[c] == dim
 
 
 @pytest.mark.parametrize("valid", argvalues=[True, False], ids=["Valid", "Invalid"])
@@ -133,9 +143,9 @@ def test_base_init(valid, test_class, test_data):
     -----------
     TestData:
         Class inheriting from :class:`Data`.
-    data: 
+    data:
         Numpy array.
-    **kwargs: 
+    **kwargs:
         Coordinate arrays passed with the names
         of the coordinates in the class attribute :attr:`coords`.
 
@@ -146,7 +156,7 @@ def test_base_init(valid, test_class, test_data):
     """
     TestClass = test_class
     data, coord1, coord2, coord3 = test_data
-    expected_n = MappingProxyType({'dim1': data.shape[0], 'dim2': data.shape[1]})
+    expected_n = MappingProxyType({"dim1": data.shape[0], "dim2": data.shape[1]})
     if valid:
         obj = TestClass(data=data, coord1=coord1, coord2=coord2, coord3=coord3)
         np.testing.assert_array_equal(obj.data, data)
@@ -160,17 +170,17 @@ def test_base_init(valid, test_class, test_data):
 
 # ----------------------------------------------------------------------------
 class TestData(Data):
-    dim2coord = MappingProxyType({
-        'dim1': frozenset(['coord_x']),
-        'dim2': frozenset(['coord_y'])
-    })
+    dim2coord = MappingProxyType({"dim1": frozenset(["coord_x"]), "dim2": frozenset(["coord_y"])})
+
     def __init__(self, data=None, **kwargs):
         if data is None:
-            data = np.zeros((len(kwargs['coord_x']), len(kwargs['coord_y'])))
+            data = np.zeros((len(kwargs["coord_x"]), len(kwargs["coord_y"])))
         super().__init__(data, **kwargs)
+
     @property
     def path(self):
-        return 'path/to/data'
+        return "path/to/data"
+
 
 def test_init_two_ways():
     """
@@ -183,4 +193,3 @@ def test_init_two_ways():
         It implements its own :meth:`__init__` method.
         If data is provided, it calls the base class :meth:`__init__`.
     """
-    
