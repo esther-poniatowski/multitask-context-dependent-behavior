a38ed5b9f4d7e38581b086e63a6209dfdaed2fdc
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-07-16 14:44:01 +0200

    docs: Improve the description of the data class strategy.

--- src/mtcdb/data_structures/__init__.py
+++ src/mtcdb/data_structures/__init__.py
@@ -5,11 +5,12 @@
 
 Classes representing data structures used in the analysis.
 
-Each custom data structure stands as a type in itself.
-It encapsulates the data, constraints and properties inherent to the specific data,
-and the relevant methods to manipulate it.
-Each data type can be used in docstrings and type hints of other objects.
-It is documented here once for all, as a central reference.
+Each data structure stands as a custom type in itself. A data structure object encapsulates the
+actual data values, the constraints and properties inherent to this specific data set, and the
+relevant methods to manipulate it.
+
+Each data structure provides a central documentation. Data types can be used in docstrings and type
+hints of other objects.
 
 Modules
 -------
@@ -19,10 +20,8 @@ Implementation
 --------------
 **Abstract Base Classes and Concrete Implementations**
 
-:class:`Data` is an abstract base class which defines the interface used
-uniformly across the whole package to interact with data.
-Each subclass implements one version for a specific data structure.
-It stores :
+:class:`Data` (abstract base class) defines the interface used uniformly across the package to
+interact with data. Each subclass implements one version for a specific data structure. It stores :
 
 - Actual data to analyze
 - Coordinates representing dimension labels
@@ -32,37 +31,33 @@ It stores :
 
 Data structures delegate certain operations to external classes :
 
-- Path generation
-  Each data class is associated to one subclass of :class:`PathManager`.
-- Loading and Saving data from and to files
-  Each data class can be associated to a :class:Loader and/or Saver subclass
-  depending on the file format.
+- :class:`PathManager` subclasses for path generation.
+- :class:`Loader` and :class:`Saver` subclasses for loading/saving data in specific file formats.
 
-To interact with those external classes, data classes store instances
-of the appropriate objects among their attributes.
+To interact with those external classes, data classes store instances of the appropriate objects
+among their attributes.
 
 **Interactions with Coordinates - Dependency Injection**
 
-Coordinate objects have to be instantiated outside of the data constructors
-and passed as arguments.
-This constraint reduces coupling between Data and Coordinate classes.
+Coordinate objects have to be instantiated outside of the data constructors and passed as arguments.
+This constraint reduces coupling between :class:`Data` and :class:`Coordinate` classes.
 
 **Data Instantiation**
 
-A data structure can be created via two pathways :
+A data structure can be obtained via two pathways :
 
-- Loading data from a file
-  Method: ``load``
-  Use cases:
-      - To recover data at the start of a processing pipeline
-      - To visualize data contents
++-----------+--------------------------------------+--------------------------------------+
+|           | Loading data from a file             | Creating data from scratch           |
++===========+======================================+======================================+
+| Approach  | ``Data(...).load()``                 | ``Data(...)``                        |
+|           | with minimal arguments               | with additional arguments to pass    |
+|           | to build the path to the file        | data and coordinate values           |
++-----------+--------------------------------------+--------------------------------------+
+| Use Cases | - To recover data at the start       | - To create the final product at     |
+|           |   of a processing pipeline           |   the end of a processing pipeline   |
+|           | - To visualize data contents         | - To test functions with custom data |
++-----------+--------------------------------------+--------------------------------------+
 
-- Creating data from scratch
-  Method: `Data(...)`
-  (direct call to the constructor with appropriate arguments)
-  Use cases:
-      - To create the final product at the end of a processing pipeline
-      - To test functions with custom data
 
 See Also
 --------
