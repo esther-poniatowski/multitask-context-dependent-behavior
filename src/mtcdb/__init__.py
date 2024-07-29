a144c280971a49c8fe71749936cdfd34a812afa5
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-07-29 16:23:42 +0200

    refactor(init): Remove dunder variables which are now specified in meta.env
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

--- src/mtcdb/__init__.py
+++ src/mtcdb/__init__.py
@@ -27,17 +27,5 @@ test_mtcdb: Tests for the whole package.
 """
 # pylint: disable=unused-variable
 # pylint: disable=unused-wildcard-import
-__author__ = "Esther Poniatowski"
-__maintainer__ = "Esther Poniatowski"
-__credits__ = [
-    "Laboratoire des Systemes Perceptifs de l'ENS",
-    "Laboratoire de Neurosciences Cognitives et Computationnelles de l'ENS",
-]
-__email__ = "esther.poniatowski@ens.psl.eu"
-__copyright__ = "Copyright 2025, Multi-Task Context-Dependent Behavior"
-__license__ = "GPL"
-__version__ = "1.0.0"
-__status__ = "Development"
-__docformat__ = "numpydoc"
 
 from mtcdb.constants import *
