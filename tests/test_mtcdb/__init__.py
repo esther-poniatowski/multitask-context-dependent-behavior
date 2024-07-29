dcacf339e6a0f1f5f25a5ff1b5ebec2906b31d15
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-07-29 08:47:08 +0200

    style(tests): Correct line length
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

--- tests/test_mtcdb/__init__.py
+++ tests/test_mtcdb/__init__.py
@@ -19,7 +19,6 @@ See Also
 
 Notes
 -----
-This test package is imported by Sphinx to extract docstrings 
-for documenting tests. This documentation provides additional
-examples and explanations for the tested modules and functions.
+This test package is imported by Sphinx to extract docstrings for documenting tests. This
+documentation provides additional examples and explanations for the tested modules and functions.
 """
