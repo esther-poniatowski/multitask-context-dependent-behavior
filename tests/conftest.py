06df06d40c4b22da8d24043635bda265fc6f12da
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-08-12 15:55:02 +0200

    refactor(tests): Move the report in the docs directly.
    Use an environment variable to specify the paths.
    
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,10 +18,8 @@ REPORT_NAME = 'test_results'
 REPORT_EXTENSION = '.xml'
 
 def pytest_configure(config):
-	# Get the root directory of the workspace
-	root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    # Set the reports directory path
-	reports_dir = os.path.join(root_dir, 'reports')
+	# Get the path to the reports directory in the workspace (environment variable)
+	reports_dir = os.environ.get('REPORTS_DIR', 'reports')
 	# Ensure the reports directory exists
 	os.makedirs(reports_dir, exist_ok=True)
 	if config.option.xmlpath is None:
