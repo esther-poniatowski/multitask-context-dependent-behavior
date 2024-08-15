f142fbed95ccb4142a39c7cc6f086424b3d3eb02
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-08-15 10:19:23 +0200

    test(path_system): Adapt tests to the new package structure
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

--- tests/test_utils/test_path_system/test_storage_rulers.py
+++ tests/test_utils/test_path_system/test_storage_rulers.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-:mod:`test_core.test_utils.test_path_managers_impl` [module]
+:mod:`test_core.test_utils.test_path_system.test_storage_rulers` [module]
 
 See Also
 --------
-:mod:`utils.io.path_managers.impl`: Tested module.
+:mod:`utils.path_system.storage_rulers.base`: Tested module.
+:mod:`utils.path_system.storage_rulers.impl`: Tested module.
 """
+import os
 
 import pytest
 
-from utils.io.path_managers.impl import RawSpkTimesPath, FiringRatesPath
+from utils.path_system.storage_rulers.base import PathRuler
+from utils.path_system.storage_rulers.impl import RawSpkTimesPath, FiringRatesPath
+
+
+def test_get_root(tmp_path):
+    """
+    Test :meth:`PathRuler.get_root` for the default value of the data root directory.
+
+    Test Inputs
+    -----------
+    tmp_path : Path
+        Temporary directory created by pytest fixture. It is exported as an environment variable
+        under the name "DATA_DIR" in the current shell.
+
+    Expected Output
+    ---------------
+    Value of the environment variable "DATA_DIR".
+    """
+    data_dir = tmp_path
+    os.environ["DATA_DIR"] = str(data_dir)
+    assert PathRuler.get_root() == data_dir, "Root directory not retrieved"
 
 
 def test_rawspktimes_path(tmp_path):
