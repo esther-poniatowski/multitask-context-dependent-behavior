71a3b2eafc2310cdddeb83107ffe93a3ab69158a
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-08-15 10:14:05 +0200

    refactor(init): Create/update `init` files to reflect the new package structure
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

---
+++ src/utils/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+:mod:`utils` [package]
+
+Package for Utilities in Multi-Task Context-Dependent Behavior PhD (2022-2025).
+
+Modules
+-------
+
+
+Sub-Packages
+------------
+:mod:`io`
+:mod:`path_system`
+:mod:`misc`
+
+See Also
+--------
+:mod:`test_utils`: Tests for the package.
+"""
