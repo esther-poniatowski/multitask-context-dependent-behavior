dee98521eebb1bd6a481295add11f879106f7059
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-08-31 17:10:57 +0200

    refactor(deplot): Convert everything into a class
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

--- src/tasks/network/deploy.py
+++ src/tasks/network/deploy.py
@@ -16,9 +16,9 @@ Usage
 Arguments
 ---------
 --env-path : str
-    Path to the `.env` file where the network credentials are defined (see :func:`load_network_config`).
+    Path to the `.env` file storing the network credentials (see :meth:`load_network_config`).
 --sync-map-path : str
-    Path to the `sync-map.yml` file where the sync map is defined (see :attrs:`Deployer.sync_map`).
+    Path to the `sync-map.yml` file where the sync map is defined (see :meth:`load_sync_map`).
 
 Notes
 -----
@@ -26,47 +26,51 @@ Structure of the input sync map in the `sync-map.yml` file:
 
 .. code-block:: yaml
 
-    directories:
-        - local: path/to/local/directory1
-        remote: path/to/remote/directory1
-        - local: path/to/local/directory2
-        remote: path/to/remote/directory2
-    files:
-        - local: path/to/local/file1
-        remote: path/to/remote/file1
-        - local: path/to/local/file2
-        remote: path/to/remote/file2
+     - source: path/to/source/directory/
+       destination: path/to/destination/directory/
+     - source: path/to/source/file.ext
+       destination: path/to/destination/file.ext
+     - source: path/to/source/file.ext
+       destination: path/to/destination/directory/
+     - source: path/to/source/file.ext
+       destination: path/to/destination/directory/new_name.ext
 
 
 Structure of the output sync map processed by the :class:`Deployer` class:
 
 .. code-block:: python
 
-    {
-        'directories': [
-            {'local': 'path/to/local/directory1', 'remote': 'path/to/remote/directory1'},
-            {'local': 'path/to/local/directory2', 'remote': 'path/to/remote/directory2'}
-        ],
-        'files': [
-            {'local': 'path/to/local/file1', 'remote': 'path/to/remote/file1'},
-            {'local': 'path/to/local/file2', 'remote': 'path/to/remote/file2'}
-        ]
-    }
+    [
+        {"source": "path/to/source/directory/", "destination": "path/to/destination/directory/"},
+        {"source": "path/to/source/file.ext", "destination": "path/to/destination/file.ext"},
+        {"source": "path/to/source/file.ext", "destination": "path/to/destination/directory/"},
+        {"source": "path/to/source/file.ext", "destination": "path/to/destination/directory/new_name.ext"}
+    ]
 
 """
 import argparse
 import os
 from pathlib import Path
 import subprocess
-from typing import Dict, Union, Optional
+from typing import Dict, List, Union, Optional
 
 from dotenv import load_dotenv
-import yaml
+
+from utils.io_data.loaders.impl import LoaderYAML
+from utils.io_data.formats import TargetType
 
 
 class Deployer:
     """
-    Deploy files to a remote server.
+    Deploy files to a remote server using a sync map.
+
+    Class Attributes
+    ----------------
+    env_keys : Dict[str, str]
+        Mapping of the class attributes to the corresponding environment variables in the `.env`
+        file.
+    attr_types : Dict[str, type]
+        Mapping of the class attributes to their respective types.
 
     Attributes
     ----------
@@ -76,42 +80,57 @@ class Deployer:
         IP address or hostname of the remote server.
     root_path : Path
         Path of the root directory of the workspace on the remote server.
-    sync_map : dict
-        Mapping of local paths (files and directories) to the corresponding remote paths.
-        The structure and content of the directory corresponds to the `sync-map.yml` file.
-        Two keys: `directories` and `files`, each containing a list of dictionaries.
-        Within each dictionary, two keys `local` and `remote` specify the respective paths.
+    sync_map : List of Dict[str, str]
+        Mapping of local paths (files and directories) to the corresponding remote paths as
+        specified in the `sync-map.yml` file.
+        Structure: List of dictionaries, each representing a pair of source and destination paths.
+        Each dictionary holds two keys: `source` and `destination`, to specify the respective paths.
 
     Methods
     -------
     :meth:`deploy`
     :meth:`transfer`
+    :meth:`load_sync_map`
+    :meth:`load_network_config`
 
     See Also
     --------
     :class:`pathlib.Path`
     :mod:`subprocess`
     """
-    def __init__(self,
-                 user: str,
-                 host:str,
-                 root_path: Union[Path, str],
-                 sync_map: Dict[str, str]
+
+    env_keys = {"user": "USER", "host": "HOST", "root_path": "ROOT"}
+    attr_types = {"user": str, "host": str, "root_path": Path}
+
+    def __init__(
+        self,
+        user: Optional[str] = None,
+        host: Optional[str] = None,
+        root_path: Optional[Union[Path, str]] = None,
+        sync_map: Optional[List[Dict[str, str]]] = None,
     ):
         self.user = user
         self.host = host
-        self.root_path = Path(root_path)
-        self.sync_map = sync_map
+        if root_path is not None:
+            root_path = Path(root_path).resolve()  # absolute path
+        self.root_path = root_path
+        self.sync_map = sync_map if sync_map is not None else []  # avoid TypeError
 
     def deploy(self):
         """Transfer files and directories to the remote server based on the sync map."""
-        for key in ['directories', 'files']:
-            for map in self.sync_map.get(key, []):
-                self.transfer(map['local'], map['remote'])
+        for paths in self.sync_map:
+            self.transfer(paths["source"], paths["destination"])
 
-    def transfer(self, local_path, path):
+    def transfer(self, source_path, destination_path):
         """
         Transfer one file or directory to the remote server though remote synchronization.
+
+        Arguments
+        ---------
+        source_path : str
+            Path to the file or directory to transfer.
+        destination_path : str
+            Path to the destination file or directory on the remote server.
 
         Raises
         ------
@@ -122,7 +141,7 @@ class Deployer:
         --------
         :meth:`Path.resolve`:
             Resolve the full path of a file or directory, i.e., expand the path.
-            Used here to get the full path of the local file or directory.
+            Used here to get the full path of the source file or directory.
         :meth:`subprocess.run`
             Execute a command in a subprocess.
             Used here to run the rsync command.
@@ -136,92 +155,84 @@ class Deployer:
             `-v` (verbose)  : Display the progress of the transfer
             `-z`            : Compress data during the transfer
         """
-        local_full_path = Path(local_path).resolve()
-        remote_full_path = f"{self.user}@{self.host}:{self.root_path}/{path}"
-        command = f"rsync -avz {local_full_path} {remote_full_path}"
-        try:
-            subprocess.run(command, check=True)
-            print(f"Successfully synced {local_full_path} to {remote_full_path}")
-        except subprocess.CalledProcessError as e:
-            raise RuntimeError(f"Failed to sync {local_full_path} to {remote_full_path}") from e
+        source_full_path = str(Path(source_path).resolve())
+        destination_full_path = f"{self.user}@{self.host}:{self.root_path}/{destination_path}"
+        command = ["rsync", "-avz", source_full_path, destination_full_path]
+        subprocess.run(command, check=True)
 
+    def load_sync_map(self, path: Union[Path, str]):
+        """
+        Load the sync map from a YAML in the attribute :attr:`Deployer.sync_map`.
 
-def load_network_config(path: Union[Path, str]) -> Dict[str, str]:
-    """
-    Extract user, host, and root path from a `.env` file.
+        Arguments
+        ---------
+        path : str
+            Path to the YAML file containing the sync map.
 
-    Arguments
-    ---------
-    path : str
-        Path to the `.env` file containing the network settings.
+        See Also
+        --------
+        :class:`utils.io_data.loaders.impl.LoaderYAML`
+        """
+        loader = LoaderYAML(path, tpe=TargetType.DICT)
+        self.sync_map = loader.load()
 
-    Returns
-    -------
-    connection_settings : dict
-        Dictionary containing the user, host, and root path.
+    def load_network_config(self, path: Union[Path, str]):
+        """
+        Extract user, host, and root path from a `.env` file and set corresponding attributes.
 
-    Raises
-    ------
-    ValueError
-        If the `.env` file does not contain the required network settings.
+        Arguments
+        ---------
+        path : str
+            Path to the `.env` file containing the network settings.
 
-    See Also
-    --------
-    :func:`load_dotenv`
-        Load environment variables from a file in a dictionary of the variables.
-    """
-    load_dotenv(path)
-    connection_settings = {
-        'user': os.getenv('USER', ''),
-        'host': os.getenv('HOST', ''),
-        'root_path': os.getenv('ROOT', '')
-    }
-    missing = [key for key, value in connection_settings.items() if not value]
-    if missing:
-        raise ValueError("Missing network settings in .env file: {}".format(', '.join(missing)))
-    return connection_settings
+        Raises
+        ------
+        ValueError
+            If the `.env` file does not contain the required network settings.
 
-
-def load_sync_map(path):
-    """
-    Load the sync map from a YAML.
-
-    Arguments
-    ---------
-    path : str
-        Path to the YAML file containing the sync map (see :attr:`Deployer.sync_map`).
-
-    Returns
-    -------
-    dict
-        Dictionary containing the sync map.
-
-    See Also
-    --------
-    :func:`yaml.safe_load`
-        Parse a YAML file and return the corresponding Python object.
-    """
-    with open(path, 'r') as file:
-        return yaml.safe_load(file)
+        See Also
+        --------
+        :func:`load_dotenv`
+            Load environment variables from a file in a dictionary of the variables.
+        """
+        path = Path(path).resolve()  # absolute path
+        load_dotenv(path)
+        connection_settings = {key: os.getenv(value) for key, value in self.env_keys.items()}
+        for key, value in connection_settings.items():
+            if not value:
+                raise ValueError(f"Missing network setting in .env file: {key}")
+            setattr(self, key, self.attr_types[key](value))
 
 
 def main():
     """Execute the deployment process."""
     parser = argparse.ArgumentParser(description="Deploy files to a remote server.")
-    parser.add_argument('--env-path', type=str, required=True, help='Path to the .env file.')
-    parser.add_argument('--sync-map-path', type=str, required=True, help='Path to the sync-map.yml file.')
+    parser.add_argument(
+        "--env-path",
+        type=argparse.FileType("r"),  # check if the file exists and is readable
+        required=True,
+        help="Path to the .env file storing the credentials for the remote server.",
+    )
+    parser.add_argument(
+        "--sync-map-path",
+        type=argparse.FileType("r"),
+        required=True,
+        help="Path to the sync-map.yml file storing path correspondences local-remote.",
+    )
     args = parser.parse_args()
 
-    network_config = load_network_config(args.env_path)
-    sync_map = load_sync_map(args.sync_map_path)
-
-    deployer = Deployer(
-        user=network_config['user'],
-        host=network_config['host'],
-        root_path=network_config['root_path'],
-        sync_map=sync_map
-    )
+    deployer = Deployer()
+    # Load configurations from the files
+    deployer.load_network_config(args.env_path)
+    deployer.load_sync_map(args.sync_map_path)
+    # Close files explicitly after loading configurations
+    with args.env_path as env_file:
+        deployer.load_network_config(env_file.name)
+    with args.sync_map_path as sync_map_file:
+        deployer.load_sync_map(sync_map_file.name)
+    # Deploy files to the remote server
     deployer.deploy()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
