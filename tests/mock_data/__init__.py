44cf09316bbd0dc473f3cd61d48d270de1f1f949
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-09-06 15:41:31 +0200

    refactor(tests): Reorganize mock data in tests to centralize all
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

---
+++ tests/mock_data/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+:mod:`mock_data` [package]
+
+Modules
+-------
+:mod:`match_content`
+:mod:`python_objects`
+
+Notes
+-----
+Keep the prefix `mock_` for the module name to prevent exclusion from the git repository, since the
+pattern `data` is included in the `.gitignore` file.
+"""
