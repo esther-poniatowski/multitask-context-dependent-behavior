1e4587e14eb58dd612a9eb9bf344ae6762b06044
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-09-11 08:28:08 +0200

    feat(parse): Initialize code to parse experimental events (future coding)
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

---
+++ src/core/parse/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+:mod:`core.parse` [package]
+
+Parse the raw data to extract metadata.
+
+Raw data consists in `.csv` files obtained from `.m` files (Matlab scripts).
+
+Files
+-----
+Input
+    Raw data from each session.
+Output
+    ?
+
+Modules
+-------
+
+See Also
+--------
+"""
