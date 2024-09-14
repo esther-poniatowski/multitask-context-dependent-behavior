da2db8246a2149d47ad75362dc72ffe5468baa4b
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-09-14 17:10:40 +0200

    fix(init): Restore the content of the core init file
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

---
+++ src/core/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+:mod:`core` [package]
+
+Package for Multi-Task Context-Dependent Behavior PhD work (2022-2025).
+
+The whole package performs data analysis of neuronal recordings in ferret brains, collected in two
+context-dependent decision making tasks.
+
+Modules
+-------
+:mod:`constants`
+
+Sub-Packages
+------------
+:mod:`entities`
+:mod:`coordinates`
+:mod:`data_structures`
+:mod:`preprocess`
+
+See Also
+--------
+:mod:`test_core`: Tests for the whole package.
+"""
+# pylint: disable=unused-variable
+# pylint: disable=unused-wildcard-import
+
+from core.constants import *
