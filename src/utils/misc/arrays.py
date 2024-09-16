451012c1e524e4a38fed84bbdd6de87b2254c0ec
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-09-16 11:55:55 +0200

    feat(utils): Create a function to create empty arrays with a number of dimensions.
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

---
+++ src/utils/misc/arrays.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+:mod:`utils.misc.arrays` [module]
+
+Utilities for arrays.
+
+Functions
+---------
+:func:`unique`
+
+"""
+import numpy as np
+import numpy.typing as npt
+
+
+def create_empty_array(n_dims: int, dtype: npt.DTypeLike) -> np.ndarray:
+    """
+    Create an empty array with a given number of dimensions and data type.
+
+    Each dimension has size zero (i.e. length 0).
+
+    Parameters
+    ----------
+    n_dims: int
+        Number of dimensions.
+    dtype: numpy.dtype
+        Data type.
+
+    Returns
+    -------
+    numpy.ndarray
+        Empty array with the specified number of dimensions and data type.
+    """
+    return np.empty((0,) * n_dims, dtype=dtype)
