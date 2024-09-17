a9fc6b1ac8908f9d706ac636f287c805ca798b42
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-09-17 12:48:50 +0200

    style(processors): Minor syntax
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

--- src/core/processors/preprocess/bootstrap.py
+++ src/core/processors/preprocess/bootstrap.py
@@ -49,7 +49,7 @@ Implementation
    each unit.
 """
 # Disable error codes for attributes which are not detected by the type checker:
-# - Configuration and data attributes are initialized by the base class constructor.
+# (configuration and data attributes are initialized by the base class constructor)
 # mypy: disable-error-code="attr-defined"
 # pylint: disable=no-member
 # pylint: disable=attribute-defined-outside-init
