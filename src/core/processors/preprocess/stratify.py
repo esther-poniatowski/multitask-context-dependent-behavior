d236dbb900428b80ea17138bd751f02b5d36ee24
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-10-08 16:22:19 +0200

    refactor(stratifier): Allow passing feature combinations to control the stratum labels.
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

--- src/core/processors/preprocess/stratify.py
+++ src/core/processors/preprocess/stratify.py
@@ -11,7 +11,7 @@ Classes
 # (configuration and data attributes are initialized by the base class constructor)
 # pylint: disable=useless-parent-delegation
 
-from typing import List, TypeAlias, Union, Any, Tuple, Dict
+from typing import overload, List, TypeAlias, Union, Any, Tuple, Dict, Literal
 
 import numpy as np
 
@@ -24,6 +24,9 @@ Strata: TypeAlias = np.ndarray[Tuple[Any], np.dtype[np.int64]]
 Features: TypeAlias = List[np.ndarray[Tuple[Any], np.dtype[Union[np.int64, np.float64, np.str_]]]]
 """Type alias for a list of feature arrays."""
 
+FeatComb: TypeAlias = List[Tuple[Union[np.int64, np.float64, np.str_], ...]]
+"""Type alias for the unique combinations of feature values which define each stratum."""
+
 
 class Stratifier(Processor):
     """
@@ -35,30 +38,43 @@ class Stratifier(Processor):
     - Arguments: Input data to process, passed to the `process` method (base class).
     - Returns: Output data after processing, returned by the `process` method (base class).
 
+    Class Attributes
+    ----------------
+    UNASSIGNED_STRATUM : int
+        Default value for unassigned stratum labels. See method `stratify`.
+
+    Attributes
+    ----------
+    strata_def : FeatComb, optional
+        Unique combinations of the feature values which define each single stratum.
+        Length: ``n_strata``, number of strata labels.
+        Shape of each element: ``(n_features,)``, number of features.
+        The index of each tuple corresponds to the stratum label.
+        If not provided, it is computed from the input features.
+
     Arguments
     ---------
     features : Features
-        Features to consider to stratify the samples (e.g., task, context, stimulus).
+        Features associated to the samples (e.g., task, context, stimulus).
         Length: ``n_features``.
         Shape of each element (feature): ``(n_samples,)``.
         .. _features:
+    return_comb : bool, optional
+        If `True`, return the feature combinations used to define the strata.
 
     Returns
     -------
     strata : Strata
         Stratum labels of the samples. Shape: ``(n_samples,)``.
         .. _strata:
-    feat_comb : Features, optional
-        Unique combinations of the feature which define each single stratum.
-        Length: ``n_features``.
-        Shape of each element: ``(n_strata,)``, with ``n_strata`` the number of strata (labels).
-        .. _feat_comb:
+    strata_def : FeatComb, optional
+        See the argument :ref:`strata_def`.
 
     Methods
     -------
     `stratify`
     `get_feature_types`
-    `convert_feat_comb`
+    `convert_strata_def`
 
     Examples
     --------
@@ -68,11 +84,10 @@ class Stratifier(Processor):
     ...             np.array([0.1, 0.1, 0.2], dtype=np.float64),
     ...             np.array(["A", "A", "B"], dtype=np.str_)]
     >>> stratifier = Stratifier()
-    >>> strata, feat_comb = stratifier.process(features=features)
+    >>> strata, strata_def = stratifier.process(features=features)
     >>> print(strata)
     [0 0 1]
-    >>> print(feat_comb)
-    [array([1, 2]), array([0.1, 0.2]), array(['A', 'B'], dtype='<U1')]
+    >>> print(strata_def)
 
     See Also
     --------
@@ -81,60 +96,100 @@ class Stratifier(Processor):
     """
 
     is_random: bool = False
+    UNASSIGNED_STRATUM: int = -1
 
-    def __init__(self):
-        super().__init__()  # no configuration parameters
+    def __init__(self, strata_def: FeatComb | None = None):
+        super().__init__()
+        if strata_def is not None:
+            self._validate_configuration(strata_def)
+        self.strata_def = strata_def
+
+    # --- Validation Methods -----------------------------------------------------------------------
+
+    def _validate_configuration(self, strata_def: FeatComb) -> None:
+        """
+        Validate the configuration parameters of the processor if provided.
+
+        Raises
+        ------
+        ValueError
+            If the feature combinations do not have the same length.
+            If the feature combinations are not unique.
+        """
+        n_features = len(strata_def[0])
+        if not all(len(comb) == n_features for comb in strata_def):
+            raise ValueError("Unequal number of features in the feature combinations.")
+        if len(strata_def) != len(set(strata_def)):
+            raise ValueError("Non-unique feature combinations in `strata_def`.")
 
     def _pre_process(self, **input_data: Any) -> Dict[str, Any]:
         """
-        Validate the features to be used for stratification.
+        Validate the features to consider for stratification.
 
         Raises
         ------
         ValueError
             If the number of samples is not equal across features.
+            If the number of feature arrays does not match the number of features in combinations.
         """
         features = input_data.get("features", [])
         n_samples = [len(feat) for feat in features]
         if not all(n == n_samples[0] for n in n_samples):
             raise ValueError(f"Unequal number of samples across features: {n_samples}")
+        if self.strata_def is not None:  # if provided
+            n_features = len(self.strata_def[0])
+            if len(features) != n_features:
+                raise ValueError("Unequal number of features in `strata_def` and `features`.")
         return input_data
 
-    def _process(self, **input_data: Any) -> Tuple[Strata, Features]:
+    # --- Processing Methods -----------------------------------------------------------------------
+
+    @overload
+    def _process(
+        self, return_comb: Literal[True] = True, **input_data: Any
+    ) -> Tuple[Strata, FeatComb]: ...
+
+    @overload
+    def _process(self, return_comb: Literal[False] = False, **input_data: Any) -> Strata: ...
+
+    def _process(
+        self, return_comb=False, **input_data: Any
+    ) -> Union[Strata, Tuple[Strata, FeatComb]]:
         """Implement the template method called in the base class `process` method."""
         features = input_data["features"]
-        strata, feat_comb_raw = self.stratify(features)
-        feat_types = self.get_feature_types(features)
-        feat_comb = self.convert_feat_comb(feat_comb_raw, feat_types)
-        return strata, feat_comb
+        if self.strata_def is None:
+            strata_def = self.compute_combinations(features)
+        else:
+            strata_def = self.strata_def
+        strata = self.stratify(features, strata_def)
+        if return_comb:
+            return strata, strata_def
+        else:
+            return strata
 
-    def stratify(self, features: Features) -> Tuple[Strata, np.ndarray]:
+    def compute_combinations(self, features: Features) -> FeatComb:
         """
-        Compute stratum labels based on unique combinations of features.
+        Compute the unique combinations of features to define the strata.
+
+        Output format: List of `n_strata` tuples, each containing the feature values which define a
+        stratum. The index of each tuple corresponds to the stratum label.
 
         Arguments
         ---------
         features : Features
             See the argument :ref:`features`.
+        feat_types : List[type]
+            See the return value :ref:`feat_types`.
 
         Returns
         -------
-        strata : Strata
-            See the return value :ref:`strata`.
-        feat_comb_raw : np.ndarray
-            Unique combinations of the feature values, in a raw format.
-            Shape: ``(n_strata, n_features)``.
-            Data type: ``np.str_`` or ``np.float64`` or ``np.int64``, depending on the types of the
-            features. Prioritized order: ``np.str_`` -> ``np.float64`` -> ``np.int64``.
-            .. _feat_comb_raw:
+        strata_def : FeatComb
+            See the return value :ref:`strata_def`.
 
         Implementation
         --------------
-        1. Stack the feature values to identify unique combinations.
-        2. Identify the unique combinations of the feature values.
-        3. Assign a stratum label to each unique combination.
-        4. Assign the stratum labels to the samples based on the values of their features.
-        5. Convert the unique combinations of features to their initial format.
+        1. Identify the unique combinations of values among the features.
+        2. Convert the unique combinations of features to their initial format.
 
         See Also
         --------
@@ -143,47 +198,74 @@ class Stratifier(Processor):
             the resulting stacked array will upcast data types to an array of `str`.
             Resulting shape (here): ``(n_samples, n_features)``.
         :func:`numpy.unique`
-            Find the unique elements of an array.
-            Output: ``(feat_comb_raw, strata)``
-            Parameter `return_inverse=True`: Return the *indices* of the unique combinations in the
-            original array, which are used to assign the stratum labels to the samples.
+            Give the unique elements of an array, *sorted* in ascending or lexicographic order.
+            Output: ``unique_feat``.
+            Shape: ``(n_strata, n_features)``.
+            Data type: ``np.str_`` or ``np.float64`` or ``np.int64``, depending on the types of the
+            features. Prioritized order: ``np.str_`` -> ``np.float64`` -> ``np.int64``.
         """
+        # Identify unit combinations of features
         feature_stack = np.column_stack(features)  # rows: samples, columns: features
-        feat_comb_raw, strata = np.unique(feature_stack, axis=0, return_inverse=True)
-        return strata, feat_comb_raw
+        unique_feat = np.unique(feature_stack, axis=0)
+        # Convert to appropriate types
+        f_types = [feat.dtype.type for feat in features]
+        strata_def = [tuple(tpe(val) for val, tpe in zip(comb, f_types)) for comb in unique_feat]
+        return strata_def
 
-    def get_feature_types(self, features: Features) -> List[type]:
+    def stratify(self, features: Features, strata_def: FeatComb) -> Strata:
         """
-        Retrieve the types of the features.
+        Compute stratum labels based on unique combinations of features.
 
         Arguments
         ---------
         features : Features
             See the argument :ref:`features`.
+        strata_def : FeatComb
+            See the attribute `strata_def`.
 
         Returns
         -------
-        feat_types : List[type]
-            Types of the features. Length: ``n_features``.
-            .. _feat_types:
-        """
-        return [feat.dtype.type for feat in features]
+        strata : Strata
+            See the return value :ref:`strata`.
 
-    def convert_feat_comb(self, feat_comb_raw: np.ndarray, feat_types: List[type]) -> Features:
+        Implementation
+        --------------
+        1. Stack the feature values to form a 2-D array of shape ``(n_samples, n_features)``.
+        2. For each combination, assign a stratum label (index of the combination) to all the
+           samples which match the combination.
+        3. Check for invalid combinations in the input features, identified by a remaining value -1.
         """
-        Convert the unique combinations of features to their initial format.
+        feature_stack = np.column_stack(features)
+        strata = np.full(len(feature_stack), -1, dtype=np.int64)  # initialize with -1
+        for i, comb in enumerate(strata_def):
+            mask = np.all(feature_stack == comb, axis=1)
+            strata[mask] = i
+        if np.any(strata == self.UNASSIGNED_STRATUM):
+            raise ValueError("Invalid combinations in `features`, not present in `strata_def`")
+        return strata
+
+    # --- Companion Methods ------------------------------------------------------------------------
+
+    def get_stratum_indices(self, stratum_label: int, strata: Strata) -> np.ndarray:
+        """
+        Get the absolute indices of the samples in a specific stratum.
 
         Arguments
         ---------
-        feat_comb_raw : np.ndarray
-            See the return value :ref:`feat_comb_raw`.
-        feat_types : List[type]
-            See the return value :ref:`feat_types`.
+        stratum_label : int
+            Label of the stratum to extract.
+        strata : Strata
+            Stratum labels assigned to the samples. Shape: ``(n_samples,)``.
 
         Returns
         -------
-        feat_comb : Features
-            See the return value :ref:`feat_comb`.
+        idx : np.ndarray
+            Indices of the subset of samples from the stratum in the global dataset.
+
+        Implementation
+        --------------
+        Find the indices of the trials in the stratum of interest:
+        ``idx_in_stratum = np.where(strata == label)[0]``
+        Extract the first (unique) element of the tuple since strata is one-dimensional.
         """
-        feat_comb: Features = [feat_comb_raw[:, i].astype(tpe) for i, tpe in enumerate(feat_types)]
-        return feat_comb
+        return np.where(strata == stratum_label)[0]
