ae778410a782c2c8b3d1d8f2b73ae4a0d7c73d61
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-10-09 13:01:05 +0200

    refactor(): Raname `RawSpk` into `RawSpike`
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

--- src/utils/storage_rulers/__init__.py
+++ src/utils/storage_rulers/__init__.py
@@ -19,7 +19,7 @@ Define a File Path:
     root_data = '/path/to/data/directory'
     unit = 'avo052a-d1'
     session = 'avo052a04_p_PTD'
-    pm = RawSpkTimesPath(root_data)
+    pm = RawSpikeTimesPath(root_data)
     path = pm.get_path(unit, session)
     print(path)
     # Output: /path/to/data/directory/raw/avo052a-d1/avo052a04_p_PTD
