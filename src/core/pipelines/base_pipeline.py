49a1e6b6c0782975592f775f35646fb4f2fc6bb4
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-10-25 09:54:55 +0200

    feat(pipeline): Initialize the `pipelines` module
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

---
+++ src/core/pipelines/base_pipeline.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+`core.pipelines.base_pipeline` [module]
+
+Classes
+-------
+`Pipeline`
+
+Notes
+-----
+Each subclass of `Pipeline` should  inherits from this class.
+"""
