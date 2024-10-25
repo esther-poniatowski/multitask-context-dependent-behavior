f5acb62c3896751d65c0b0a3c6337df636ff75e5
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-10-25 09:07:26 +0200

    feat(build_session_trials): WIP - Add one module to store trials properties in one session
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

---
+++ src/core/builders/build_session_trials.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+`core.builders.build_session_trials` [module]
+
+Classes
+-------
+`RasterBuilder`
+
+Notes
+-----
+-
+"""
+# pylint: disable=missing-function-docstring
+
+from typing import List, Tuple, Optional, Any, TypeAlias
+
+import numpy as np
+
+from core.builders.base_builder import DataBuilder
+from core.coordinates.base_coord import Coordinate
+from core.coordinates.exp_condition import CoordStim
+from core.coordinates.exp_structure import CoordBlock, CoordSlot
+from core.coordinates.time import CoordTimeEvent
+from core.coordinates.trials import CoordError
+from core.data_structures.core_data import CoreData
+from core.data_structures.session_events import SessionEvents
+from core.data_structures.session_trials import SessionTrials
+
+
+class SessionTrialsBuilder(DataBuilder[SessionEvents, SessionTrials]):
+    """
+    Build a `SessionTrials` data structure from a `SessionEvents` data structure.
+
+    Class Attributes
+    ----------------
+    product_class : type
+        See the base class attribute.
+    TMP_DATA : Tuple[str]
+        See the base class attribute.
+
+    Processing Attributes
+    ---------------------
+    session_events : SessionEvents
+        Raw meta data about the events which occurred in the session.
+
+    Methods
+    -------
+    `build` (implementation of the base class method)
+    """
+
+    product_class = SessionTrials
+    TMP_DATA = ("session_events",)
+
+    def __init__(self) -> None:
+        # Call the base class constructor: declare empty product and internal data
+        super().__init__()
+        # Store configuration parameters
+
+        # Declare attributes to store inputs and intermediate results
+        self.session_events: Optional[SessionEvents] = None
+
+    def build(
+        self,
+        session_id: Optional[str] = None,
+        session_events: Optional[SessionEvents] = None,
+        **kwargs,
+    ) -> SessionTrials:
+        """
+        Implement the base class method.
+
+        Parameters
+        ----------
+        session_id : str
+            Identifier of the session.
+        session_events : SessionEvents
+            See the attribute `session_events`.
+
+        Returns
+        -------
+        product : SessionTrials
+            Data structure product instance.
+        """
+        assert session_id is not None and session_events is not None
+        # Store inputs
+        self.session_events = session_events
+        # Preliminary set up
+
+        # Initialize the data structure with its metadata (base method)
+        self.initialize_data_structure(session_id=session_id)
+        # Add core data values to the data structure
+        data = self.construct_core_data()
+        self.add_data(data)
+        # Add coordinates in the data structure
+        block: CoordBlock
+        slot: CoordSlot
+        stim: CoordStim
+        t_on: CoordTimeEvent
+        t_off: CoordTimeEvent
+        t_warn: CoordTimeEvent
+        t_end: CoordTimeEvent
+        error: CoordError
+        self.add_coords(
+            block=block,
+            slot=slot,
+            stim=stim,
+            t_on=t_on,
+            t_off=t_off,
+            t_warn=t_warn,
+            t_end=t_end,
+            error=error,
+        )
+        return self.get_product()
+
+    # --- Shape and Dimensions ---------------------------------------------------------------------
+
+    @property
+    def n_trials(self) -> int:
+        """Number of trials which occurred in the session."""
+        return 0  # TODO
+
+    # --- Preliminary Operations -------------------------------------------------------------------
+
+    # --- Construct Core Data ----------------------------------------------------------------------
+
+    def construct_core_data(self) -> CoreData:
+        """
+        Construct the core data array containing the spike trains of the unit in the trials.
+
+        Returns
+        -------
+        data : CoreData
+            Data array containing the spike times.
+            Shape: ``(n_trials, n_spk_max)``.
+            .. _data:
+
+        Implementation
+        --------------
+        1. Initialize the core data array with empty values.
+        2.
+
+        See Also
+        --------
+        `DataBuilder.initialize_core_data`
+            Initialize the core data array with empty values (parent method).
+
+        """
+        shape = (self.n_trials,)
+        data = self.initialize_core_data(shape)  # parent method
+        # TODO
+        return data
+
+    # --- Construct Coordinates --------------------------------------------------------------------
+
+    def construct_coord(self, name, coord_class: type[Coordinate]) -> Coordinate:
+        """
+        Construct a new coordinate  for the trials dimension containing session's information.
+
+        Each session is associated with one recording number, one task and one context, and yields
+        as many number of elements as the total number of trials it contains.
+
+        Parameters
+        ----------
+        name : str
+            Name of the coordinate to build.
+        coord_class : type[Coordinate]
+            Class of the coordinate to build. Used to convert the array of values into a coordinate
+            object of the appropriate type.
+
+        Returns
+        -------
+        coord : Coordinate
+            Coordinate for the trial dimension in the data structure product.
+        """
+        coord: Coordinate
+        # TODO
+        return coord
