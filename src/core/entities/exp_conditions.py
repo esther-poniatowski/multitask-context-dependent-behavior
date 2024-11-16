f61391f513a75a19e1afa09ecbefa4a9da7f0aff
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-11-16 09:46:05 +0100

    refactor(exp_cond, coords): Extract the coordinate related logic to a `CoordManager` class
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

--- src/core/entities/exp_conditions.py
+++ src/core/entities/exp_conditions.py
@@ -8,22 +8,22 @@ Classes representing the experimental conditions of the behavioral paradigm.
 Classes
 -------
 `ExpCondition`
+`ExpConditionUnion`
+`PipelineCondition`
 """
 from types import MappingProxyType
 
-from typing import Self, Iterable, List, Dict, Type, Set, FrozenSet, Mapping
+from typing import Self, Iterable, List, Dict, Type, FrozenSet, Mapping
 from itertools import product
-import warnings
-
-import numpy as np
 
 from core.entities.exp_factors import ExpFactor
-from core.coordinates.exp_factor_coord import CoordExpFactor
 
 
 class ExpCondition:
     """
-    Experimental condition defined by the combination of several experimental factors, among:
+    Experimental condition defined by the combination of several experimental factors.
+
+    Usual experimental factors include:
 
     - task
     - attentional state
@@ -33,8 +33,8 @@ class ExpCondition:
 
     Attributes
     ----------
-    factors : List[str]
-        Register of the attributes names for all specified factors.
+    registry : List[str]
+        Registry of the attributes names for all specified factors.
     factor_types : Dict[Type[ExpFactor]], str]
         Mapping from the factor types (classes) and their attribute names.
 
@@ -50,8 +50,7 @@ class ExpCondition:
 
     Notes
     -----
-    Partial conditions can be defined by specifying only the factors of interest. The remaining
-    factors are set to `None` by default.
+    Partial conditions can be defined by specifying only the factors of interest.
 
     Warning
     -------
@@ -68,10 +67,6 @@ class ExpCondition:
     >>> exp_cond_1 = ExpCondition(task=task, category=category, behavior=behavior)
     >>> exp_cond_1
     ExpCondition(task=PTD, category=R, behavior=Go)
-
-    Initialize directly with strings:
-
-    >>> exp_cond_2 = ExpCondition(task='CLK', attention='p', category='T')
 
     Retrieve the task:
 
@@ -90,18 +85,18 @@ class ExpCondition:
     """
 
     def __init__(self, **factors: Dict[str, ExpFactor]) -> None:
-        self.factors: List[str] = []
+        self.registry: List[str] = []
         self.factor_types: Dict[Type[ExpFactor], str] = {}
         for name, factor in factors.items():
             if not isinstance(factor, ExpFactor):
                 raise TypeError(f"Invalid argument for ExpCondition: {name} not ExpFactor")
             setattr(self, name, factor)
-            self.factors.append(name)
+            self.registry.append(name)
             self.factor_types[factor.__class__] = name
 
     def __iter__(self):
         """
-        Iterate over the factors of the experimental condition.
+        Iterate over the factors specified in the experimental condition.
 
         Yields
         ------
@@ -110,28 +105,37 @@ class ExpCondition:
         fact : ExpFactor
             Factor instance.
         """
-        for name in self.factors:
+        for name in self.registry:
             yield name, getattr(self, name)
 
     def __repr__(self) -> str:
         return f"ExpCondition({', '.join(f'{name}={fact}' for name, fact in self)})"  # use __iter__
 
-    def get_factor_name(self, factor_type: Type[ExpFactor]) -> str | None:
+    def get_factor(self, factor_type: Type[ExpFactor]) -> str | None:
         """
-        Get the attribute name corresponding to an experimental factor class.
+        Get the stored factor value corresponding to an experimental factor class.
 
         Arguments
         ---------
         factor_type : Type[ExpFactor]
-            Class of the experimental factor for which to retrieve the attribute name.
+            Class of the experimental factor for which to retrieve the value.
 
         Returns
         -------
-        name : str | None
-            Name of the attribute corresponding to the factor type.
+        factor : ExpFactor
+            Value of the factor if present in the experimental condition instance.
             None if the factor type is not present in the experimental condition instance.
+
+        Examples
+        --------
+        >>> exp_cond = ExpCondition(task=Task('PTD'), attention=Attention('a'))
+        >>> exp_cond.get_factor(Task)
+        Task('PTD')
         """
-        return self.factor_types.get(factor_type, None)
+        name = self.factor_types.get(factor_type, None)
+        if name is not None:
+            return getattr(self, name, None)
+        return None
 
     @staticmethod
     def combine_factors(*selected_factors: Iterable[ExpFactor] | ExpFactor) -> List["ExpCondition"]:
@@ -156,7 +160,7 @@ class ExpCondition:
         Generate all possible conditions for two tasks, a fixed attentional state, two stimuli and
         both behaviors:
 
-        >>> exp_conds = ExpCondition.generate_conditions(
+        >>> exp_conds = ExpCondition.combine_factors(
         ...     tasks=[Task("PTD"), Task("CLK")],
         ...     attentions=Attention("a"),
         ...     categories=[Category("R"), Category("T")],
@@ -198,12 +202,19 @@ class ExpCondition:
         See Also
         --------
         `Entity.__eq__`
+
+        Examples
+        --------
+        >>> exp_cond_1 = ExpCondition(task=Task('PTD'), attention=Attention('a'))
+        >>> exp_cond_2 = ExpCondition(task=Task('CLK'), attention=Attention('a'))
+        >>> exp_cond_1 == exp_cond_2
+        False
         """
         # Check type ExpCond
         if not isinstance(other, self.__class__):
             return False
         # Check similar names
-        names_1, names_2 = set(self.factors), set(other.factors)
+        names_1, names_2 = set(self.registry), set(other.registry)
         if names_1 != names_2:
             return False
         # Check factors equality
@@ -221,7 +232,7 @@ class ExpCondition:
         int
             Hash value of the experimental condition instance.
         """
-        return hash(tuple(getattr(self, name) for name in self.factors))
+        return hash(tuple(getattr(self, name) for name in self.registry))
 
     def __add__(self, other: Self) -> "ExpConditionUnion":
         """
@@ -229,102 +240,19 @@ class ExpCondition:
 
         Examples
         --------
-        >>> exp_cond_1 = ExpCondition(task='PTD', attention='a', category='R')
-        >>> exp_cond_2 = ExpCondition(task='CLK', attention='p', category='T')
+        >>> exp_cond_1 = ExpCondition(task=Task('PTD'), attention=Attention('a'))
+        >>> exp_cond_2 = ExpCondition(task=Task('CLK'), attention=Attention('p'))
         >>> union = exp_cond_1 + exp_cond_2
-        >>> union.conditions
-        [ExpCondition(task='PTD', attention='a', category='R'),
-         ExpCondition(task='CLK', attention='p', category='T')]
+        >>> union.get()
+        [ExpCondition(task='PTD', attention='a'),
+         ExpCondition(task='CLK', attention='p')]
         """
         return ExpConditionUnion(self, other)
-
-    def match(self, *coords: CoordExpFactor) -> np.ndarray:
-        """
-        Generate a boolean mask to index a set of samples based on a condition.
-
-        Arguments
-        ---------
-        coords : CoordExpFactor
-            Coordinates of experimental factors for each sample.
-
-        Returns
-        -------
-        mask : np.ndarray
-            Boolean mask to index samples that match the condition.
-            .. _mask:
-
-        Raises
-        ------
-        UserWarning
-            If the coordinate class is not handled for the types of factors stored in the
-            experimental condition instance.
-            If the type of factor associated to a coordinate is duplicated among the coordinates.
-        ValueError
-            If the number of samples in the provided coordinates is inconsistent.
-
-        Examples
-        --------
-        >>> exp_cond = ExpCondition(task='PTD', attention='a', category='R')
-        >>> task_coord = np.array(['PTD', 'CLK', 'PTD'])
-        >>> attn_coord = np.array(['a', 'p', 'a'])
-        >>> categ_coord = np.array(['R', 'T', 'R'])
-        >>> mask = exp_cond.match(task=task_coord, attn=attn_coord, categ=categ_coord)
-        >>> mask
-        array([ True, False,  True])
-
-        See Also
-        --------
-        `Coordinate.get_entity`
-            Get the type of entity associated with the coordinate, which is expected to be an
-            `ExpFactor` entity for `CoordExpFactor` instances.
-        """
-        # Filter coordinates based on the condition factors
-        # Ignore coordinate if:
-        # - it types is not handled by the condition
-        # - its type has already been encountered in a previous coordinate (duplicate type)
-        ignored: List[int] = []
-        seen_types: Set[Type[ExpFactor]] = set()
-        for i, coord in enumerate(coords):
-            entity = coord.get_entity()  # retrieve the entity associated with the coordinate
-            if entity not in self.factor_types or entity in seen_types:
-                ignored.append(i)
-        for i in ignored:
-            warnings.warn(f"Ignored coordinates: {coords[i].__class__}", UserWarning)
-        valid_idx = [i for i in range(len(coords)) if i not in ignored]
-        # Initialize the mask with all True values
-        n_samples = [len(coord) for coord in coords if coord not in ignored]
-        if len(set(n_samples)) > 1:
-            raise ValueError(f"Inconsistent number of samples across coordinates: {n_samples}")
-        mask = np.ones(n_samples[0], dtype=bool)
-        # Apply matching conditions for each coordinate only for specified instance factors
-        for coord in [coords[i] for i in valid_idx]:
-            name = self.get_factor_name(entity)  # attribute name for factor type
-            if name is not None:  # always verified here, checked to silence type checker errors
-                match_value = getattr(self, name)
-                if match_value is not None:  # idem
-                    mask &= coord == match_value
-        return mask
-
-    def count(self, *coords: CoordExpFactor) -> int:
-        """
-        Count the number of samples that match the condition.
-
-        Arguments
-        ---------
-        coords : CoordExpFactor
-
-        Returns
-        -------
-        n_samples : int
-            Number of samples matching the condition.
-        """
-        mask = self.match(*coords)
-        return np.sum(mask)
 
 
 class ExpConditionUnion:
     """
-    Union of experimental conditions.
+    Union of experimental conditions, behaving like a list of conditions.
 
     Attributes
     ----------
@@ -335,22 +263,27 @@ class ExpConditionUnion:
     -------
     `get`
     `__iter__`
-    `match`
-    `count`
+
+    Raises
+    ------
+    TypeError
+        If any of the arguments is not an `ExpCondition` instance
 
     Examples
     --------
-    >>> exp_cond_1 = ExpCondition(task='PTD', attention='a', category='R')
-    >>> exp_cond_2 = ExpCondition(task='CLK', attention='p', category='T')
-    >>> exp_cond_3 = ExpCondition(task='PTD', category='R', behavior='Go')
+    >>> exp_cond_1 = ExpCondition(task=Task('PTD'), attention=Attention('a'))
+    >>> exp_cond_2 = ExpCondition(task=Task('CLK'), attention=Attention('p'))
+    >>> exp_cond_3 = ExpCondition(category=Category('R'), behavior=Behavior('Go'))
     >>> union = ExpConditionUnion(exp_cond_1, exp_cond_2, exp_cond_3)
     >>> union.get()
-    [ExpCondition(task='PTD', attention='a', category='R'),
-     ExpCondition(task='CLK', attention='p', category='T'),
-     ExpCondition(task='PTD', category='R', behavior='Go')]
+    [ExpCondition(task='PTD', attention='a'),
+     ExpCondition(task='CLK', attention='p'),
+     ExpCondition(category='R', behavior='Go')]
     """
 
     def __init__(self, *exp_conds: ExpCondition) -> None:
+        if any(not isinstance(cond, ExpCondition) for cond in exp_conds):
+            raise TypeError(f"Invalid argument for ExpConditionUnion: {exp_conds} not ExpCondition")
         self.exp_conds = list(exp_conds)
 
     def get(self) -> List[ExpCondition]:
@@ -358,63 +291,16 @@ class ExpConditionUnion:
         return self.exp_conds
 
     def __iter__(self):
-        """Iterate over the conditions in the union."""
+        """Iterate over the conditions in the union, providing each condition one by one."""
         return iter(self.exp_conds)
-
-    def match(self, **coords: CoordExpFactor) -> np.ndarray:
-        """
-        Generate a boolean mask to index samples that match any of the conditions.
-
-        Arguments
-        ---------
-        coords : CoordExpFactor
-
-        Returns
-        -------
-        mask : np.ndarray
-            Boolean mask to index samples that match any of the conditions.
-
-        Examples
-        --------
-        >>> exp_cond_1 = ExpCondition(task='PTD', attention='a', category='R')
-        >>> exp_cond_2 = ExpCondition(task='CLK', attention='a', category='T')
-        >>> union = exp_cond_1 + exp_cond_2
-        >>> task_coord = np.array(['PTD', 'CLK', 'PTD'])
-        >>> attn_coord = np.array(['a', 'p', 'a'])
-        >>> categ_coord = np.array(['R', 'T', 'R'])
-        >>> mask = union.match(task=task_coord, attn=attn_coord, categ=categ_coord)
-        >>> mask
-        array([ True, False,  True])
-        """
-        # Get the masks for each condition in the union using the match method of ExpCondition
-        all_masks = [cond.match(**coords) for cond in self.exp_conds]
-        # Combine all masks with a logical OR operation
-        mask = np.logical_or.reduce(all_masks)
-        return mask
-
-    def count(self, **coords: CoordExpFactor) -> int:
-        """
-        Count the number of samples that match any of the conditions.
-
-        Arguments
-        ---------
-        coords : CoordExpFactor
-
-        Returns
-        -------
-        n_samples : int
-            Number of samples matching any of the conditions.
-        """
-        mask = self.match(**coords)
-        return np.sum(mask)
 
 
 class PipelineCondition(ExpCondition):
     """
     Base class for experimental conditions tailored to specific pipelines.
 
-    Attributes
-    ----------
+    Class Attributes
+    ----------------
     REQUIRED_FACTORS : Mapping[str, FrozenSet[ExpFactor]], default={}
         Experimental factors required by the pipeline.
         Keys: Names of the factors to use as attributes for the experimental conditions.
@@ -423,8 +309,7 @@ class PipelineCondition(ExpCondition):
 
     Methods
     -------
-    generate:
-        Generate all conditions for the pipeline based on the specified factors.
+    `generate`
 
     Raises
     ------
