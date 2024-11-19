176c561e6dbd4233cd454b947443d5205aae0b27
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-11-19 08:13:51 +0100

    feat(steps): Add a class hierarchy for steps in the pipeline (WIP)
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

---
+++ src/core/steps/concrete_steps.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+`core.processors.base_step` [module]
+
+Classes
+-------
+`Step`
+
+"""
+from typing import Any, List, Optional
+
+from core.steps.base_step import Step
+from core.entities.bio_info import Area, Unit
+from utils.io_data.base_loader import Loader
