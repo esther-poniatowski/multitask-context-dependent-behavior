e7453b39855eb7be1b63b5616a42bdc7635d6f8c
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-11-23 20:01:12 +0100

    refactor(candidates): Inherit from UserList
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

--- src/core/composites/candidates.py
+++ src/core/composites/candidates.py
@@ -5,38 +5,44 @@
 
 Classes
 -------
-`Excluder`
+`Candidates`
 
 """
-
-from typing import List, Iterable, Callable, TypeVar, Generic
+from collections import UserList
+from typing import Iterable, Callable, TypeVar, Generic
 
 T = TypeVar("T")
 """Type variable representing the type of elements in the candidate set."""
 
 
-class Candidates(Generic[T]):
+class Candidates(UserList, Generic[T]):
     """
     Utilities to exclude elements from a set from various criteria.
 
-    Attributes
-    ----------
-    candidates : List[T]
+    Arguments
+    ---------
+    candidates : Iterable[T]
         Elements from which some members might be excluded. Input candidates can be provided under
         the form of any iterable, but are converted to a list for internal processing.
+
+    Attributes
+    ----------
+    data : List[T]
+        Internal list containing the candidate set.
 
     Methods
     -------
     `exclude`
     `filter`
+    `filter_by_associated`
+
+    See Also
+    --------
+    `collections.UserList`: Inherit from this class to provide list-like behavior.
     """
 
     def __init__(self, candidates: Iterable[T]) -> None:
-        self.candidates = list(candidates)
-
-    def get(self) -> List[T]:
-        """Get the candidate set, possibly after exclusions."""
-        return self.candidates
+        super().__init__(candidates)
 
     def exclude(self, intruders: Iterable) -> None:
         """
@@ -55,7 +61,7 @@ class Candidates(Generic[T]):
         >>> candidates.get()
         [1, 3, 5]
         """
-        self.candidates = [element for element in self.candidates if element not in intruders]
+        self.data = [element for element in self.data if element not in intruders]
 
     def filter(self, predicate: Callable) -> None:
         """
@@ -75,4 +81,26 @@ class Candidates(Generic[T]):
         >>> candidates.get()
         [2, 4]
         """
-        self.candidates = [element for element in self.candidates if predicate(element)]
+        self.data = [element for element in self.data if predicate(element)]
+
+    def filter_by_associated(self, values: Iterable, predicate: Callable) -> None:
+        """
+        Exclude elements based on a predicate applied to associated values.
+
+        Arguments
+        ---------
+        values : Iterable
+            Values associated with the candidates, in the same order.
+        predicate : Callable
+            Function that takes a single argument and returns a boolean, indicating whether an
+            associated value satisfies the condition.
+
+        Examples
+        --------
+
+        >>> candidates = Candidates(['a', 'b', 'c', 'd'])
+        >>> candidates.filter_by_associated([1, 2, 3, 4], lambda x: x % 2 == 0)
+        >>> candidates.get()
+        ['b', 'd']
+        """
+        self.data = [element for element, value in zip(self.data, values) if predicate(value)]
