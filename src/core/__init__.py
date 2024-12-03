3aa49d9053d74a5fe864670b42aa0e0e3e280492
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2024-12-03 15:44:03 +0100

    docs(*): Explain the hierarchy of classes
    Signed-off-by: Esther Poniatowski <eponiatowski@clipper.ens.psl.eu>

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -10,49 +10,52 @@ context-dependent decision-making tasks.
 
 Notes
 -----
-This core package contains two types of classes:
+To promote modularity, scalability, and maintainability in the analysis workflow, the package is
+structured into distinct types of classes. Those are organized in a hierarchical structure.
 
-- Static classes: Attributes, Coordinates, Data Structures.
-- Dynamic classes: Processors, Builders, Pipelines.
+Two main types of classes are defined based on their role in the analysis workflow:
 
-Both types are organized in a hierarchical structure to promote modularity, scalability, and
-maintainability in the analysis workflow.
+- Static classes: `Attribute`, `Dimensions`, `DataComponent` (`Coordinate`, `CoreData`),
+  `DataStructure`, `Container`.
+- Dynamic classes: `Processor`, `Factory`, `Builder`, `Pipeline`.
 
 Static Classes
 ^^^^^^^^^^^^^^
 Static classes form the building blocks of the analysis workflow. They are organized into three
 hierarchical categories:
 
-1. **Attributes**: Low-level classes representing the basic data types manipulated in the analysis.
-   They provide a central reference and documentation for important aspects of the experiment.
-2. **Coordinates**: Intermediate-level classes representing the dimensions of the data. Each
-   coordinate is typically associated with an attribute, as it describes one experimental variable.
-3. **Data Structures**: High-level classes representing the milestones of the analysis. They
-   usually store core data values (to be analyzed), coordinates, and metadata.
+1. **Attributes**: Low-level classes (enum-like) representing the basic data types manipulated in
+   the analysis. They provide a central reference and documentation for important aspects of the
+   experiment or analysis (e.g., brain areas, experimental conditions, units, folds...).
+2. **Data Components**: Intermediate-level classes (numpy-array-like) containing the values to
+   analyse or their associated labels. They are further refined between `CoreData` and `Coordinate`.
+   Each coordinate is typically associated with an attribute, as it describes one experimental
+   variable or analysis label.
+3. **Data Structures**: High-level classes (dictionary-like) obtained at the milestones of the
+   analysis. They store several data components (core data values, coordinates), and metadata.
 
 Dynamic Classes
 ^^^^^^^^^^^^^^^
 Dynamic classes perform the actual analysis tasks and manipulate the static classes. They are
-organized into three hierarchical levels: Processors, Builders, and Pipelines.
+organized into several hierarchical categories:
 
-1. **Processors**: Low-level classes performing specific computations or tasks on basic data types
-   (e.g., NumPy arrays). Each processor encapsulates one distinct operation, breaking down complex
-   tasks into manageable components.
-2. **Builders**: Intermediate-level classes responsible for producing complex data structures. Each
-   builder executes multiple processors to generate the components required by a specific data
+1. **Processors**: Low-level classes performing focused operations on basic data types (e.g., NumPy
+   arrays).
+2. **Factories**: Intermediate-level classes responsible for creating data components. Each
+   factory executes multiple processors to generate one or several coupled components.
+3. **Builders**: High-level classes responsible for producing complex data structures. Each
+   builder executes multiple factories to generate the components required by a specific data
    structure, and stores those components within the data structure object.
-3. **Pipelines**: High-level classes orchestrating the entire analysis workflow. Each pipeline
-   manages data loading, saving, and invokes builders to generate milestone objects for the
-   analysis.
+4. **Pipelines**: Director classes orchestrating an entire workflow. Each pipeline manages data
+   loading, saving, and invokes builders to generate milestone objects for the analysis.
 
+Package Structure
+^^^^^^^^^^^^^^^^^
+This package is structured into sub-packages, each containing one or several related class(es).
 
-Design Choices
-^^^^^^^^^^^^^^
-This package is structured into sub-packages, each corresponding to a type of class: Attributes,
-Coordinates, Data Structures, Processors, Builders, and Pipelines.
-
-Each sub-package contains a base class that defines the interface, common attributes, and methods
-shared across the concrete implementations within that sub-package.
+Class hierarchies typically conform to a specific interface defined in a base class. This interface
+specified common attributes and methods shared across the concrete implementations within that
+hierarchy.
 
 Disabled Warnings
 ^^^^^^^^^^^^^^^^^
@@ -79,9 +82,12 @@ Modules
 Sub-Packages
 ------------
 `attributes`
+`data_components`
 `coordinates`
 `data_structures`
+`composites`
 `processors`
+`factories`
 `builders`
 `pipelines`
 
