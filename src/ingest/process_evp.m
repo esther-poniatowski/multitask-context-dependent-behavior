32c083326c502e6234f8bff9b94a2148308de14d
Esther Poniatowski <55659704+esther-poniatowski@users.noreply.github.com>
2025-01-05 20:18:15 +0100

    refactor(*): Delete src directory

--- src/ingest/process_evp.m
+++
@@ -1,5 +0,0 @@
-function process_evp(input_path, output_path)
-    [~,~,rAtot,ATrialIdxtot] = evpread(input_path,'auxchans',1);
-    T = table(rAtot, ATrialIdxtot);
-    writetable(T, output_path);
-end
