0661ba53c430ed3e9d732355c0497dc24a6d12f3
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-05-12 20:52:52 +0200

    feat(src): Turn `mtcdb_etl` into a package

---
+++ src/mtcdb_etl/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+mtcdb_etl
+=========
+
+Multi-Task-Context-Dependent-Behavior - Extraction - Transformation - Loading
+
+Part of the MTCDB project dedicated to the ETL process.
+
+Modules
+-------
+remote_access
+    Manages remote access across multiple servers involved in the ETL process.
+data_transfer
+    Manages the transfer of raw data between different servers.
+ingest
+    Prepares data for processing.
+"""
