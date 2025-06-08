d1f934ab1f675de1ed4695288b8bb0db5403c7d0
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-06-08 12:54:01 +0200

    feat(__main__): Add entry point

--- src/mtcdb/__main__.py
+++ src/mtcdb/__main__.py
@@ -1,16 +1,16 @@
 """
-Command-line entry point for the `{{ package_name }}` package.
+Command-line entry point for the `mtcdb` package.
 
 Usage
 -----
-To invoke the {{ package_name }} package:
+To invoke the package::
 
-    python -m {{ package_name }}
+    python -m mtcdb
 
 
 See Also
 --------
-{{ package_name }}.cli: Command-line interface module for the {{ package_name }} package.
+mtcdb.cli: Command-line interface module for the package.
 """
 from .cli import app
 
